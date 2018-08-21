1928b22698b073e78ee957752ee26adadc393173
Julien Ducrocq <julien.ducrocq77@gmail.com>
2018-08-21 16:59:17 +0200

    Associations and Validations done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :duck
   belongs_to :user
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :status, presence: true, inclusion: { in: %w[waiting pending finished] }
+  validates :duck, presence: true
+  validates :user, presence: true
 end
