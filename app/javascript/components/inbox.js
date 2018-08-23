eb288ae5d48b2135be2fcdad2c800dbe5af9aea7
Arthur GAUTHIER <arthurgauthier.as@gmail.com>
2018-08-23 18:43:28 +0200

    pending_review

--- app/javascript/components/inbox.js
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById("comment-form");
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const name = `<h3>${document.getElementById("your-name").value}</h3>`;
-  list.insertAdjacentHTML("beforeend", name);
-  const message = `<li>${document.getElementById("your-message").value}</li>`;
-  list.insertAdjacentHTML("beforeend", message);
-  send();
-});
---
+++ app/javascript/components/review.js
@@ -0,0 +1,5 @@
+document.getElementById('review-form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const message = document.getElementById('message-object').value;
+  const name = document.getElementById('message-content').value;
+});
