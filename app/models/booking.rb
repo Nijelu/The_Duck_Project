7c89aea9da870b02c8d8dc815b852c7626a2be41
Gonzague Gru <gonzague@agricool.co>
2018-08-23 18:55:08 +0200

    validate date possible and pass the status in finished

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,4 +7,20 @@ class Booking < ApplicationRecord
   validates :status, presence: true, inclusion: { in: %w[waiting pending finished] }
   validates :duck, presence: true
   validates :user, presence: true
+  validate :validate_end_date_before_start_date
+  validate :expiration_date_cannot_be_in_the_past
+
+  def validate_end_date_before_start_date
+    if end_date && start_date
+      errors.add(:end_date, "End date cannot be before start date") if end_date < start_date
+    end
+  end
+
+
+  def expiration_date_cannot_be_in_the_past
+    if start_date.present? && start_date < Date.today
+      errors.add(:start_date, "can't be in the past")
+    end
+  end
+
 end
